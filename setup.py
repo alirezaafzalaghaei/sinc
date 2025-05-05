0231dcf27a9f26b99e34e4ee159ae48ba2c9b7b3
Alireza Afzal Aghaei <alirezaafzalaghaei@gmail.com>
2025-05-06 01:12:46 +0330

    first version

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='sinc',
+    version='1.0.0',
+    packages=["sinc"],
+    description="Sinc Kernel Function",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author='Alireza Afzal Aghaei',
+    author_email='alirezaafzalaghaei@gmail.com',
+    url='https://github.com/alirezaafzalaghaei/sinc',
+    python_needed='>=3.9',
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    license='GPL-3.0',
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "numba"
+    ],
+)
