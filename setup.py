2439987d85344cf32e1d40058e3b54af9b68bdc9
Alireza Afzal Aghaei <alirezaafzalaghaei@gmail.com>
2025-05-06 01:25:42 +0330

    first version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sinc',
-    version='1.0.0',
+    version='0.1.0',
     packages=["sinc"],
     description="Sinc Kernel Function",
     long_description=open('README.md').read(),
